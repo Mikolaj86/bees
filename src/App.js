f0fac7a4a0162399425f5c1f8031a7b3afdd802d
Mikolaj86 <mikolaj.okon999@gmail.com>
2019-05-24 10:06:21 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Error404 from "./error404";
+import Register from "./register";
+import Login from "./register";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 
 function App() {
   return (
     <div className="App">
-        <Error404 />
-
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -26,5 +28,34 @@ function App() {
     </div>
   );
 }
+function Menu() {
+    return (
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">App</Link>
+                    </li>
+                    <li>
+                        <Link to="/error404">Error 404</Link>
+                    </li>
+                    <li>
+                        <Link to="/register">Register</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                </ul>
 
-export default App;
+                <hr />
+
+                <Route exact path="/" component={App} />
+                <Route path="/error404" component={Error404} />
+                <Route path="/register" component={Register} />
+                <Route path="/login" component={Login} />
+            </div>
+        </Router>
+    );
+}
+export default Menu;
+// export default App;
